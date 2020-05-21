170a46bddb1e2e52170043a2178c7da9d1a50e3d
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-05-21 16:23:13 +0200

    Add initial version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module bayern.steinbrecher.DialogUtility {
+    requires javafx.controls;
+    requires java.logging;
+
+    requires org.jetbrains.annotations;
+
+    exports bayern.steinbrecher.utility;
+}
